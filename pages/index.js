a58d97467fbadc54cc8abe584625bf36e43d1289
xtzhang1984 <xtzhang1984@gmail.com>
2021-09-17 17:02:38 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Stone Zhang is Great Man!</a>
         </h1>
 
         <p className={styles.description}>
